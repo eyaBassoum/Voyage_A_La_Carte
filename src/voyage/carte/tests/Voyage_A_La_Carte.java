0519025d65cf32e595c9161b42b79c5828685423
Eya <Eya@Eya-PC>
2014-02-19 16:48:37 +0100

    bonjour

---
+++ src/voyage/carte/tests/Voyage_A_La_Carte.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package voyage.carte.tests;
+
+/**
+ *
+ * @author Eya
+ */
+public class Voyage_A_La_Carte {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
